0e6cc1964cbf4ba3e41e9e7c488cddb0306138eb
Abishek1208 <abishek082001@gmail.com>
2024-04-22 17:32:01 +0530

    files changed 1

---
+++ src/test/java/com/example/authtokenDemo/AuthTokenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authtokenDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthTokenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
